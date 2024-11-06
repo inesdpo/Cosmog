bb2029dea6a3147ffb1ab893d1fde1aa691beb9b
inesdpo <117300107+inesdpo@users.noreply.github.com>
2024-11-06 18:22:58 +0000

    light and player correction

---
+++ Assets/materials ines/LightChange.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightChange : MonoBehaviour
+{
+    [SerializeField] private string initialState ;
+    [SerializeField] private string secondaryState;
+}
