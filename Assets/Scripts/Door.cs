c078d8eccdfd661f3a5375d018c8503653b1c81e
SukainaR <rizwansukaina@gmail.com>
2025-01-09 04:21:58 +0000

    Revert "door vr working"
    This reverts commit fe302b8ea4bb22e4d727b31fcb42d75b744f2523.

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -1,69 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class Door : MonoBehaviour
 {
     [SerializeField] private float finalZRotation;
+
     private float initialZRotation;
-    [SerializeField] private float finalXRotation;
-    private float initialXRotation;
     [SerializeField] private GameObject player;
     private bool isRotated = false;
     [SerializeField] private bool playerInCollider = false;
     [SerializeField] private AudioSource opening;
     [SerializeField] private AudioSource closing;
-    [SerializeField] private InputActionProperty triggerAction;
 
     // Start is called before the first frame update
     void Start()
     {
         initialZRotation = transform.eulerAngles.z;
-        initialXRotation = transform.eulerAngles.x;
-        triggerAction.action.Enable();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (triggerAction.action.WasPressedThisFrame() && playerInCollider)
+        if (Input.GetKeyDown(KeyCode.C) && (playerInCollider == true))
         {
-            Debug.Log("Trigger pressed!");
             if (isRotated)
             {
-                RotationZ(initialZRotation);
-                RotationX(initialXRotation);
+                Rotation(initialZRotation);
                 opening.gameObject.GetComponent<AudioSource>().enabled = false;
                 closing.gameObject.GetComponent<AudioSource>().enabled = true;
                 closing.Play();
             }
             else
             {
-                RotationZ(finalZRotation);
-                RotationX(finalXRotation);
+                Rotation(finalZRotation);
                 closing.gameObject.GetComponent<AudioSource>().enabled = false;
                 opening.gameObject.GetComponent<AudioSource>().enabled = true;
                 opening.Play();
             }
             isRotated = !isRotated;
+
+
         }
+
+      
     }
 
-    public void RotationZ(float zRotation)
+    public void Rotation(float zRotation)
     {
-        Vector3 currentRotation = transform.eulerAngles;
-        currentRotation.z = zRotation;
-        transform.eulerAngles = currentRotation;
-    }
+    Vector3 currentRotation = transform.eulerAngles;
 
-    public void RotationX(float xRotation)
-    {
-        Vector3 currentRotation = transform.eulerAngles;
-        currentRotation.x = xRotation;
-        transform.eulerAngles = currentRotation;
-    }
+    currentRotation.z = zRotation;
 
+    transform.eulerAngles = currentRotation;
+    }
     public void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
@@ -71,7 +62,6 @@ public class Door : MonoBehaviour
             playerInCollider = true;
         }
     }
-
     public void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Player"))
