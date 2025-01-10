9853fb3b0556d592d34fc1b124cbbc45cf1ec6fe
inesdpo <117300107+inesdpo@users.noreply.github.com>
2025-01-10 00:12:28 +0000

    camera working

---
+++ Assets/Scripts/offset.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.XR.CoreUtils;
+using UnityEngine;
+
+public class offset : MonoBehaviour
+{
+    public float desiredHeight = 1.8f; // Desired height in meters
+
+    private XROrigin xrOrigin;
+
+    void Update()
+    {
+  
+        xrOrigin = GetComponent<XROrigin>();
+
+        if (xrOrigin != null)
+        {
+            // Get the Camera Offset Transform
+            Transform cameraOffset = xrOrigin.CameraFloorOffsetObject.transform;
+
+            // Adjust the Y position of the Camera Offset
+            Vector3 offsetPosition = cameraOffset.localPosition;
+            offsetPosition.y = desiredHeight;
+            cameraOffset.localPosition = offsetPosition;
+        }
+    }
+}
