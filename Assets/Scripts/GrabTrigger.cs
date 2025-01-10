0df212a883b7728400bc398c2e2de172cd9196d0
inesdpo <117300107+inesdpo@users.noreply.github.com>
2025-01-10 04:35:06 +0000

    recordings

--- Assets/Scripts/GrabTrigger.cs
+++ Assets/Scripts/GrabTrigger.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class GrabTrigger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private XRGrabInteractable grabInteractable;
+
+    void Awake()
     {
-        
+        grabInteractable = GetComponent<XRGrabInteractable>();
+
+        if (grabInteractable != null)
+        {
+            grabInteractable.selectEntered.AddListener(OnGrab);
+            grabInteractable.selectExited.AddListener(OnRelease);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDestroy()
     {
-        
+        if (grabInteractable != null)
+        {
+            grabInteractable.selectEntered.RemoveListener(OnGrab);
+            grabInteractable.selectExited.RemoveListener(OnRelease);
+        }
+    }
+
+    private void OnGrab(SelectEnterEventArgs args)
+    {
+        Debug.Log("Object grabbed!");
+        // Add your custom logic here, e.g., trigger an animation, sound, etc.
+    }
+
+    private void OnRelease(SelectExitEventArgs args)
+    {
+        Debug.Log("Object released!");
+        // Add your custom logic here, e.g., stop an animation, deactivate an effect, etc.
     }
 }
